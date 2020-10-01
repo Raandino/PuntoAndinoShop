f67ce9b144dc4ef3362dd4845ffcfac2ac08b900
AlexC01 <alexcuadraxxx@gmail.com>
2020-10-01 14:06:59 -0600

    ver productos por categoria

--- andinoshop/andinoshop/urls.py
+++ andinoshop/andinoshop/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from apps.core.views import frontpage
-from apps.store.views import product_detail, category
+from apps.store.views import product_detail, category_detail
 urlpatterns = [
     path('', frontpage, name='frontpage'),
-    path('categorias/', category, name='category'),
-    path('<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>/', category_detail, name='category_detail'),
     path('admin/', admin.site.urls),
 ]
--- andinoshop/apps/store/views.py
+++ andinoshop/apps/store/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 
-def category(request):
-    products = Product.objects.all()
-
-    context = {
-        'products': products
-    }
-    return render(request, 'category.html', context)
-
-
-
-def product_detail(request, slug):
+def product_detail(request, slug, category_slug):
     product = get_object_or_404(Product, slug=slug)
 
     context = {
@@ -22,3 +12,13 @@ def product_detail(request, slug):
     }
 
     return render(request, 'product_detail.html', context)
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all()
+    context = {
+        'category': category,
+        'products' : products
+    }
+
+    return render(request, 'category_detail.html', context)
